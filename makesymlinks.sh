0b0de9364308643885d78361833a7ebfba0043be
Alex Piqueras <alex@piqueras.me>
2018-06-05 10:10:52 +0200

    Added screenshots and other utilities to i3 and xdg dirs

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -9,7 +9,7 @@
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="bin vimrc vim Xresources xinitrc"    # list of files/folders to symlink in homedir
-config="i3"	# list of files/folders to symlink in config
+config="i3 user-dirs.dirs"	# list of files/folders to symlink in config
 
 ##########
 
